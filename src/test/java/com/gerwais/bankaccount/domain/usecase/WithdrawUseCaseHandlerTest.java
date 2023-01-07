5fa82b0107b7ffb05544f24d2ac9a0fc7de1b97c
Gerwais <gerwaismomo@gmail.com>
2023-01-07 23:21:40 +0100

    Withdraw apply do nothing when user has no acccount (red)

---
+++ src/test/java/com/gerwais/bankaccount/domain/usecase/WithdrawUseCaseHandlerTest.java
@@ -0,0 +1,42 @@
+package com.gerwais.bankaccount.domain.usecase;
+
+import com.gerwais.bankaccount.domain.model.Account;
+import com.gerwais.bankaccount.domain.model.User;
+import com.gerwais.bankaccount.domain.port.AccountPort;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static java.math.BigDecimal.TEN;
+import static java.math.BigDecimal.ZERO;
+import static java.time.LocalDateTime.now;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class WithdrawUseCaseHandlerTest {
+
+    @Mock
+    AccountPort accountPort;
+    WithdrawUseCaseHandler handler;
+
+    @Captor
+    ArgumentCaptor<Account> accountCaptor;
+
+    private User user;
+    private BigDecimal amount;
+
+
+
+
+}
