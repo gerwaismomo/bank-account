8b3bf5fac160f4f219397208abe7eb1dbcd55150
Gerwais <gerwaismomo@gmail.com>
2023-01-07 23:24:38 +0100

    Withdraw apply do nothing when user has no acccount (green)

---
+++ src/main/java/com/gerwais/bankaccount/domain/usecase/WithdrawUseCaseHandler.java
@@ -0,0 +1,26 @@
+package com.gerwais.bankaccount.domain.usecase;
+
+import com.gerwais.bankaccount.domain.model.Account;
+import com.gerwais.bankaccount.domain.model.User;
+import com.gerwais.bankaccount.domain.port.AccountPort;
+import com.gerwais.bankaccount.domain.port.DepositUseCase;
+import com.gerwais.bankaccount.domain.port.WithdrawUseCase;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static java.time.LocalDateTime.now;
+
+public class WithdrawUseCaseHandler implements WithdrawUseCase {
+
+    AccountPort accountPort;
+
+    public WithdrawUseCaseHandler(AccountPort accountPort) {
+        this.accountPort = accountPort;
+    }
+
+    @Override
+    public void apply(User user, BigDecimal amount) {
+
+    }
+}
