6256ce7158380a9d2c8ecce8ef20879162958e90
Gerwais <gerwaismomo@gmail.com>
2023-01-07 22:15:42 +0100

    do nothing when user has no acccount (red)

--- src/test/java/com/gerwais/bankaccount/domain/usecase/DepositUseCaseHandlerTest.java
+++ src/test/java/com/gerwais/bankaccount/domain/usecase/DepositUseCaseHandlerTest.java
@@ -1,10 +1,22 @@
 package com.gerwais.bankaccount.domain.usecase;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class DepositUseCaseHandlerTest {
 
     DepositUseCaseHandler handler;
+
+    @Test
+    void apply_shouldDoNothing_whenUserHasNoAccount() {
+        givenUserHasAccount();
+    }
+
+    private void givenUserHasAccount() {
+        given(AccountRepository.findByOwner());
+    }
 }
