489e0786fb5ecd3a2e5ccf7d4c973341d1cb8cd8
Gerwais <gerwaismomo@gmail.com>
2023-01-07 23:50:09 +0100

    History should return sorted list of operations (green)

--- src/test/java/com/gerwais/bankaccount/domain/usecase/HistoryAccountHandlerTest.java
+++ src/test/java/com/gerwais/bankaccount/domain/usecase/HistoryAccountHandlerTest.java
@@ -1,5 +1,6 @@
 package com.gerwais.bankaccount.domain.usecase;
 
+import com.gerwais.bankaccount.domain.model.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -8,8 +9,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class HistoryAccountHandlerTest {
 
 
+    private User user;
+
     @Test
     void apply_shouldReturnSortedOperations_whenUserHasOperations() {
         givenUser();
+    }
+
+    private void givenUser() {
+        user = user1();
+    }
+
+    private User user1() {
+        return new User("user1");
     }
 }
